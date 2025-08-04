099cda1ed0d95fba00bf5a82e04a090944456d7d
akicouleur <165789262+akicouleur@users.noreply.github.com>
2025-08-04 10:30:16 +0900

    TOPロゴセクション

---
+++ components/top/logo/LogoMarquee.tsx
@@ -0,0 +1,59 @@
+// components/top/logo/LogoMarquee.tsx
+"use client"
+
+import Image from "next/image"
+
+const LogoMarquee = () => {
+  // ロゴ画像の配列
+  const logos = [
+    "/top/logo/logo1.png",
+    "/top/logo/logo2.png",
+    "/top/logo/logo3.png",
+    "/top/logo/logo4.png",
+    "/top/logo/logo5.png",
+    "/top/logo/logo6.png",
+    "/top/logo/logo7.png",
+    "/top/logo/logo8.png",
+    "/top/logo/logo9.png",
+    "/top/logo/logo10.png",
+  ]
+
+  return (
+    <section className="w-full bg-white py-8 md:py-10 overflow-hidden">
+      <div className="relative">
+        {/* 無限スクロールアニメーション */}
+        <div className="flex animate-marquee whitespace-nowrap">
+          {/* 1つ目のロゴセット */}
+          {logos.map((logo, index) => (
+            <div key={`first-${index}`} className="flex-shrink-0 mx-4 md:mx-8">
+              <Image
+                src={logo}
+                alt={`Logo ${index + 1}`}
+                width={120}
+                height={60}
+                className="h-16 md:h-20 w-auto object-contain"
+              />
+            </div>
+          ))}
+          {/* 2つ目のロゴセット（無限ループ用） */}
+          {logos.map((logo, index) => (
+            <div
+              key={`second-${index}`}
+              className="flex-shrink-0 mx-4 md:mx-8"
+            >
+              <Image
+                src={logo}
+                alt={`Logo ${index + 1}`}
+                width={120}
+                height={60}
+                className="h-16 md:h-20 w-auto object-contain"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default LogoMarquee
