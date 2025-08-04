962a36fdd59aaa8de022a803504d8a71bf2d1418
akicouleur <165789262+akicouleur@users.noreply.github.com>
2025-08-04 11:30:11 +0900

    TOP Cases

--- components/top/case/Case_01.tsx
+++ components/top/case/Case_01.tsx
@@ -50,13 +50,13 @@ const Case_01 = ({ limit = 3 }: CaseProps) => {
 
   return (
     <>
-      <PageContent className="bg-bgLightBlue">
-        <section className="md:max-w-[1200px] mx-auto md:space-y-10">
-          <ContentHeadline
-            enTitle="Case study"
-            mainTitle="導入事例"
-            enTitleClassName=""
-            titleClassName=""
+      <PageContent className="">
+        <section className="md:max-w-[1200px] mx-auto md:space-y-16">
+        <ContentHeadline
+            enTitle="Cases"
+            mainTitle={`業界も領域も問わず、選ばれる理由。`}
+            enTitleClassName="text-center"
+            titleClassName="text-center"
           />
 
           <div className=" grid grid-cols-1 md:grid-cols-3 md:gap-x-10">
@@ -73,11 +73,11 @@ const Case_01 = ({ limit = 3 }: CaseProps) => {
                     />
                   )}
                 </div>
-                <div className="bg-white p-6">
+                <div className="bg-white mt-6">
                   <p className="text-lg font-bold break-words min-h-14">
                     {post.title}
                   </p>
-                  <p className="mt-2 text-[#5f5f5f] text-xs ">
+                  <p className="mt-2 text-base ">
                     {contents[0].title}
                   </p>
                 </div>
